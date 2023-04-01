ee40461662b283939e00158a805072ec5fbd510f
KSSIRI <mohamedakram.kssiri@cgi.com>
2023-04-01 13:47:27 +0000

    verification

--- src/main/java/com/example/cicdspringbootapp/CiCdSpringbootAppApplication.java
+++ src/main/java/com/example/cicdspringbootapp/CiCdSpringbootAppApplication.java
@@ -2,10 +2,17 @@ package com.example.cicdspringbootapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CiCdSpringbootAppApplication {
 
+	@GetMapping("/")
+	public String welcome(){
+		return "GGG";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdSpringbootAppApplication.class, args);
 	}
