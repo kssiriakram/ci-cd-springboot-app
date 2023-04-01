4dc54ac2a79ca358d00f14e5d66af2e23e7aa4f0
KSSIRI <mohamedakram.kssiri@cgi.com>
2023-04-01 13:26:54 +0000

    initial

---
+++ src/main/java/com/example/cicdspringbootapp/CiCdSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdSpringbootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicdspringbootapp/CiCdSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdSpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
